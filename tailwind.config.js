9073225d100d98c47c82bf981362f9396cffe06f
firshandy <firshandydwi@gmail.com>
2023-08-27 03:40:21 +0700

    update portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,8 @@ module.exports = {
       colors: {
         primary: '#f1601d',
         dark: '#0f172a',
+        wa: '#25D366',
+        ig: '#E1306C',
       },
       screens: {
         '2xl': '1320px',
